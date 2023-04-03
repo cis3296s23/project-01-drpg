8c99c34af953f707f1b251610445870452cbbf63
tuo85214temple <tuo85214@temple.edu>
2023-04-03 12:13:19 -0400

    update

--- creatures_generator.py
+++ creatures_generator.py
@@ -1,1 +1,12 @@
-#test push
+class Creature:
+    def __init__(self, character_manager, level):
+        self.character_manager = character_manager
+        with open('creatures.json', 'r') as f:
+            data = json.load(f)
+            creatures = random.choice(data['creatures'])
+            self.name = f"{creatures['adjectives'][random.randint(0, len(creatures['adjectives']) - 1)]} {creatures['nouns'][random.randint(0, len(creatures['nouns']) - 1)]}"
+            self.symbol = creatures['symbol']
+            self.level = level
+            self.health = creatures['health'] * level
+            self.attack = creatures['attack'] * level
+            self.defense = creatures['defense'] * level
