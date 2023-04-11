63491a04f41b79cb86d6bfdafa75d882d5ac6434
tuo85214temple <tuo85214@temple.edu>
2023-04-11 09:43:48 -0400

    fixed

--- armors_unit_test.py
+++ armors_unit_test.py
@@ -1,14 +1,15 @@
 import json
 import unittest
-from gear_generation import Weapon
+from gear_generation import Armor
 import os
 
 os.chdir('/Users/mikaya/Documents/GitHub/project-01-drpg/')
-class Test(unittest.TestCase):
-    def test_weapon_creation(self):
-        weapon = Weapon()
-        self.assertIsNotNone(weapon.name)
-        self.assertIsNotNone(weapon.damage)
-        self.assertIsNotNone(weapon.speed)
+
+class TestArmor(unittest.TestCase):
+    def test_armor_creation(self):
+        armor = Armor()
+        self.assertIsNotNone(armor.name)
+        self.assertIsNotNone(armor.protection)
+
 if __name__ == '__main__':
     unittest.main()
--- gear_generation.py
+++ gear_generation.py
@@ -1,9 +1,13 @@
 import json
 import random
+import os
 
 class Weapon:
     def __init__(self):
-        with open('weapons.json') as f:
+        project_root = os.path.dirname(os.path.abspath(__file__))
+        weapons_file_path = os.path.join(project_root, 'weapons.json')
+        
+        with open(weapons_file_path) as f:
             data = json.load(f)
             weapons = data['weapons']
             weapon_data = random.choice(weapons)
@@ -13,8 +17,13 @@ class Weapon:
 
 class Armor:
     def __init__(self):
-        with open('armors.json', 'r') as f:
+        project_root = os.path.dirname(os.path.abspath(__file__))
+        armors_file_path = os.path.join(project_root, 'armors.json')
+        
+        with open(armors_file_path) as f:
             data = json.load(f)
-            armors = random.choice(data['armors'])  # chooses random armor in json file
-            self.name = f"{armors}"
-            self.protection = data['armors'][armors]['protection']
+            armors = data['armors']
+            armor_data = random.choice(armors)
+            self.name = armor_data['name']
+            self.protection = armor_data['protection']
+
